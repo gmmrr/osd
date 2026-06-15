5c46e8d7c4a63b49ae0cdd32ebb85e6abe4a0abf
gmmrr <guanming091429@gmail.com>
2026-06-15 12:08:51 +0200

    add const and param

---
+++ config/constants.py
@@ -0,0 +1,9 @@
+# Minimal naming constants for the flat audio pipeline.
+
+KEY_STD = "std"
+KEY_VOCALS = "vocals"
+KEY_BACKGROUND = "background"
+KEY_NORM = "nml"
+
+EXT_WAV = ".wav"
+EXT_JSON = ".json"
